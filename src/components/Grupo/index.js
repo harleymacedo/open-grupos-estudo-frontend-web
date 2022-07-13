2cc3e61e554a50bff9f6552ea674a0e67df58ea6
harleymacedo <harley.ip@gmail.com>
2022-07-13 17:07:38 -0300

    Componente de grupo melhorado

--- src/components/Grupo/index.js
+++ src/components/Grupo/index.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles.css';
 
 export default () => {
+
+    const [grupos, setGrupos] = useState([{"nome": "nome1"}, {"nome": "nome2"}]);
+
+    useEffect( () => {
+         obterGrupos();
+    }, [] )
+    
+    const obterGrupos = async () => {
+        const result = await fetch('https://open-grupos-de-estudo-backend.herokuapp.com/grupo');
+        console.log("Chamou obterGrupos");
+        setGrupos(...grupos, result);
+    }
+
     return (
         <div className='container'>
             <p>Tela de Grupos</p>
+
+            <div>
+                { grupos.map( (value, index) => {
+                    return (
+                        <div>
+                            <p>Item atual</p>
+                            <p> { value } </p>
+                        </div>
+                    );
+                }) }
+            </div>
+
         </div>
     )
 }
