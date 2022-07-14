05e547a72e4574220ca8405953d1e204720545b8
harleymacedo <harley.ip@gmail.com>
2022-07-14 16:02:02 -0300

    React Router Dom adicionando

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './styles.css';
 
 const Login = () => {
+
+    const navegar = (local) => {
+        console.log('Navegar para ', local);
+    }
+
     return(
         <div className='container--login'>
             <h4>Entrar no sistema</h4>
             <input type='text' placeholder='Email' className='input--login'/>
             <input type='password' placeholder='Senha' className='input--login' />
-            <input type='button' value='Entrar' className='input--login input--button' />
+            <input type='button' value='Entrar' className='input--login input--button' onClick={navegar('home')} />
             <div className='login--links'>
                 <a href='https://www.heroku.com'>Cadastro</a>
                 <a href='https://www.heroku.com'>Esquecia a senha</a>
