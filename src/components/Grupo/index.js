3fc8d9d99676c5025b931ec380fe5f4af2ae795c
harleymacedo <harley.ip@gmail.com>
2022-07-23 22:45:35 -0300

    Modal de Novo Grupo adicionado

--- src/components/Grupo/index.js
+++ src/components/Grupo/index.js
@@ -1,17 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import './styles.css';
-import { Card, Button } from '@mui/material';
+import { Card, Button, TextField } from '@mui/material';
 import LogoutIcon from '@mui/icons-material/Logout';
 import EditIcon from '@mui/icons-material/Edit';
 import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import TextSnippetIcon from '@mui/icons-material/TextSnippet';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
 
 import axios from 'axios';
 
 const Grupo = () => {
 
     const [grupos, setGrupos] = useState([]);
+    const [open, setOpen] = useState(false);
     
     const obterGrupos = async () => {
         const result = await axios.get('https://open-grupos-estudo-backend.herokuapp.com/grupo');
@@ -24,13 +29,36 @@ const Grupo = () => {
         obterGrupos();
     }, [] );
 
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
         <div className='container'>
             <p style={{fontSize: 28}} >Tela de Grupos</p>
 
             <Button href='/' variant='contained' color='secondary' > <LogoutIcon /> Logout</Button>
             
-            <p> <AddCircleOutlineIcon/> </p>
+            <p> <AddCircleOutlineIcon onClick={handleClickOpen}/> </p>
+
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Novo grupo:</DialogTitle>
+                <DialogContent>
+                    <TextField id="name" label="Nome:" type="text" fullWidth variant='filled' />
+                    <TextField id="descricao" label="Descrição:" type="text" fullWidth variant='filled' />
+                    <TextField id="meta" label="Meta:" type="text" fullWidth variant='filled' />
+                    <TextField id="imagem" label='Imagem:' type="file" fullWidth variant='filled' />
+                </DialogContent>
+                <DialogActions>
+                <Button onClick={handleClose}> Cancelar </Button>
+                <Button onClick={handleClose}> Cadastrar </Button>
+                </DialogActions>
+            </Dialog>
+
             <div>
                 { grupos.map( (item, key) => {
                     return (
